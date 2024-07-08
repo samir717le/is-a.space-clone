92eb341a3fb5aec6d19a4b1257cf0b0555ae3db9
SAMIR 717 LE <158018273+samir717le@users.noreply.github.com>
2024-07-08 14:05:23 +0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
-function showMenu(id, event_id) {
+function showMenu(id, event_id, showable) {
   const element = document.getElementById(id);
   const eventElement = document.getElementById(event_id);
+  const toshow = document.getElementById(showable);
   
   if (element) { // Check if element exists
     if (element.style.display === "none") { // "none" should be a string
-      element.style.display = "inline-flex ! important";
+      element.style.display = "inline-flex";
       eventElement.style.display = "none";
+      toshow.style.display = "inline-flex";
       
     } else {
       element.style.display = "none";
       eventElement.style.display = "block";
+      toshow.style.display = "none";
     }
   } else {
     console.error(`Element with id "${id}" not found`);
